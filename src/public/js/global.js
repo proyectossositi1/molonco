d4411235f435542563b980fed82dc2949393cc61
Antonino Rodriguez <antoninorodriguez@solemti.com>
2025-05-25 10:07:36 -0700

    actualizcion español datatable

--- src/public/js/global.js
+++ src/public/js/global.js
@@ -34,7 +34,14 @@ const init = (_data = { datatable: {} }) => {
         ordering: true,
         info: true,
         autoWidth: false,
-        responsive: true
+        responsive: true,
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/2.3.1/i18n/es-ES.json'
+        },
+        dom:
+            "<'row'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
     };
 
     // Definir configuración según el tipo
